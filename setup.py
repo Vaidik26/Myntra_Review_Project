3bafcec025d98d4f8aee932576a3d12c00473947
Vaidik26 <vaidiky90@gmail.com>
2024-11-12 19:03:11 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Myntra_Scrapper",
+    version="0.0.1",
+    author="Vaidik26",
+    author_email="vaidiky90@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
